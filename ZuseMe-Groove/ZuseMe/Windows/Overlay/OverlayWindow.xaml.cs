76271af365684f8e4bcd12145156a1340786757f
Arnold Vink <info@arnoldvink.com>
2022-06-16 11:32:30 +0200

    - Set overlay window to render in software mode.

--- ZuseMe-Groove/ZuseMe/Windows/Overlay/OverlayWindow.xaml.cs
+++ ZuseMe-Groove/ZuseMe/Windows/Overlay/OverlayWindow.xaml.cs
@@ -1,14 +1,32 @@
 ﻿using ArnoldVinkCode;
 using System;
 using System.Windows;
+using System.Windows.Interop;
 
 namespace ZuseMe.Windows
 {
     public partial class WindowOverlay : Window
     {
-        public WindowOverlay()
+        //Window Initialize
+        public WindowOverlay() { InitializeComponent(); }
+
+        //Window Variables
+        private IntPtr vInteropWindowHandle = IntPtr.Zero;
+
+        //Window Initialized
+        protected override void OnSourceInitialized(EventArgs e)
         {
-            InitializeComponent();
+            try
+            {
+                //Get interop window handle
+                vInteropWindowHandle = new WindowInteropHelper(this).EnsureHandle();
+
+                //Set render mode to software
+                HwndSource hwndSource = HwndSource.FromHwnd(vInteropWindowHandle);
+                HwndTarget hwndTarget = hwndSource.CompositionTarget;
+                hwndTarget.RenderMode = RenderMode.SoftwareOnly;
+            }
+            catch { }
         }
 
         public void ShowWindowDuration(int showSeconds)
